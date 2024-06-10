831d239764505ea54ab8c5b61defa8166bd77fd8
willl85 <ngowilfried@gmail.com>
2024-06-10 20:16:48 +0200

    Update MessageScrolling Component

--- components/message-scrolling/MessageScrolling.jsx
+++ components/message-scrolling/MessageScrolling.jsx
@@ -1,17 +1,32 @@
 'use client';
-import { images } from '@/app/lib/images';
+import { useEffect, useRef } from 'react';
+import { FaStar } from 'react-icons/fa';
 import classes from './MessageScrolling.module.css';
 
 export default function MessageScrolling() {
+  const logosRef = useRef(null);
+  const starIcon = <FaStar />;
+  const messages = [
+    'Fabriqué en France',
+    'Révélez Votre Éclat',
+    'Livraison Gratuite dès 100€',
+  ];
+
+  useEffect(() => {
+    if (logosRef.current) {
+      const copy = logosRef.current.cloneNode(true);
+      console.log(copy);
+      logosRef.current.parentNode.appendChild(copy);
+    }
+  }, []);
+
   return (
     <div className={classes.scroll__container}>
-      <div className={classes.scroll__content}>
-        {images.map((image) => (
-          <img
-            key={image.caption}
-            src={`/images/${image.path}`}
-            alt={image.caption}
-          />
+      <div className={classes.scroll__content} ref={logosRef}>
+        {messages.map((mess) => (
+          <h4 key={mess}>
+            {starIcon} {mess}
+          </h4>
         ))}
       </div>
     </div>
