4e80a385da6e2c63a8a4686ebea3444606647b64
willl85 <ngowilfried@gmail.com>
2024-06-12 16:37:28 +0200

    Add metadata

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,10 @@
 import Home from '@/components/home/Home';
 
+export const metadata = {
+  title: 'Pure Glow',
+  description: 'Découvrez notre gamme de produit skincare chez Pure Glow',
+};
+
 export default function HomePage() {
   return <Home />;
 }
