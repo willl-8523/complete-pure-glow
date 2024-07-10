fc20c8881521f89fb461eef6243fec13c070c7ed
willl85 <ngowilfried@gmail.com>
2024-07-10 12:35:45 +0200

    Add icon link  on the head element

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,9 @@ import Home from '@/components/home/Home';
 export const metadata = {
   title: 'Pure Glow',
   description: 'Découvrez notre gamme de produit skincare chez Pure Glow',
+  icons: {
+    icon: 'https://www.greensoulcosmetics.com/image/catalog/logo/gsc-logo-social.png', // /public path
+  },
 };
 
 export default function HomePage() {
